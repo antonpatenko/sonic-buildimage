947f1545f23215630c2a231f7365352003e83d42
Shuotian Cheng <shuche@microsoft.com>
2016-12-16 16:43:39 -0800

    Revert "Makefile: Add platform general for platform neutral targets"

--- slave.mk
+++ slave.mk
@@ -26,7 +26,6 @@ PROJECT_ROOT = $(shell pwd)
 
 CONFIGURED_PLATFORM := $(shell [ -f .platform ] && cat .platform || echo undefined)
 PLATFORM_PATH = platform/$(CONFIGURED_PLATFORM)
-PLATFORM_GENERIC_PATH = platform/generic
 
 ###############################################################################
 ## Utility rules
@@ -52,7 +51,6 @@ distclean : .platform clean
 include $(RULES_PATH)/config
 include $(RULES_PATH)/functions
 include $(RULES_PATH)/*.mk
-include $(PLATFORM_GENERIC_PATH)/rules.mk
 ifneq ($(CONFIGURED_PLATFORM), undefined)
 include $(PLATFORM_PATH)/rules.mk
 endif
